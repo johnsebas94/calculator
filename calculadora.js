07c5ecd2a1cdd2fe80a7eef7db96ce5b46ed08a2
Sebastian Tibaquirá <johnsebas94@gmail.com>
2021-07-09 10:45:09 -0500

    Change the font-size to watch better the numbers

---
+++ calculadora.js
@@ -0,0 +1,43 @@
+let caracter = document.getElementById("caracter");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btn0 = document.getElementById("btn0");
+
+//Eventos
+btn1.onclick = function () {
+    caracter.innerHTML += "1"
+};
+btn2.onclick = function () {
+    caracter.innerHTML += "2"
+};
+btn3.onclick = function () {
+    caracter.innerHTML += "3"
+};
+btn4.onclick = function () {
+    caracter.innerHTML += "4"
+};
+btn5.onclick = function () {
+    caracter.innerHTML += "5"
+};
+btn6.onclick = function () {
+    caracter.innerHTML += "6"
+};
+btn7.onclick = function () {
+    caracter.innerHTML += "7"
+};
+btn8.onclick = function () {
+    caracter.innerHTML += "8"
+};
+btn9.onclick = function () {
+    caracter.innerHTML += "9"
+};
+btn0.onclick = function () {
+    caracter.innerHTML += "0"
+};
